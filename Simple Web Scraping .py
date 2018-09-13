923233d1606bd0e361fb9803854f9dce4341759d
Binayak Adhikari <blazebnayak@gmail.com>
2018-09-13 17:41:43 +0545

    Update on WebScrap.py

--- Simple Web Scraping .py
+++ Simple Web Scraping .py
@@ -1,13 +1,12 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-#Requesting from Web
-html=urlopen("https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=Graphic+Card&N=-1&isNodeId=1")
-
-
-soup=BeautifulSoup(html.read(),"html.parser")         #Using Beautiful Soup and Reading all html elements
-
-
-container=soup.find_all("div",{"class":"item-container  "})  #scaping class named => item-container
-
-print(len(container))          #total length of items
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as urlReq
+scrap_url = 'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=Graphic+Card&N=-1&isNodeId=1'
+urlClient = urlReq(scrap_url)                             #Requesting from Web
+html=urlClient.read()
+urlClient.close()
+page=soup(html,"html.parser")                             #Using Beautiful Soup and Reading all html elements
+containers=page.findAll("div",{"class":"item-container"})  #scraping class named => item-container
+#Now Container contains all the elememts of the class item-container
+#use for loop to get individual element seperately
+print(containers)
+print(len(containers))                                     #total length of items
